7d39df5177a329bc539ce1f0f339c707cc9de875
Kate Fox <katefox100@gmail.com>
2021-02-03 15:48:41 +0000

    Gulp sass task set up and run

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 let gulp = require('gulp');
 let cleanCSS = require('gulp-clean-css');
 let rename = require('gulp-rename');
+let sass = require('gulp-sass');
 
 gulp.task('minify-css', () => {
     return gulp.src('css/styles.css')
@@ -8,3 +9,10 @@ gulp.task('minify-css', () => {
       .pipe(rename({suffix: '.min'}))
       .pipe(gulp.dest('./css/'));
   });
+
+  gulp.task('sass', function () {
+	return gulp.src('./scss/styles.scss')
+		.pipe(sass())
+		.pipe(rename('styles.css'))
+		.pipe(gulp.dest('./css/'));
+});
